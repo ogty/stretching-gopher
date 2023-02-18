68dd4a1e16b6421e8b6c34c86136d44f4a12ddd6
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2023-02-18 20:14:16 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', async (req: any, res: any) => {
   try {
